69cf992b44beae5cc871a1ecdd20d6c984254f96
shehanhasanga <shehanhasanga.15@cse.mrt.ac.lk>
2022-01-23 07:07:54 +0530

    Initial Commit

---
+++ TestGit/TestGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestGitApp.swift
+//  TestGit
+//
+//  Created by shehan karunarathna on 2022-01-23.
+//
+
+import SwiftUI
+
+@main
+struct TestGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
